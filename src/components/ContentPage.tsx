84cb26d62e0c3af13c403d2d1354839d7afe04cb
eigsi <126054825+eigsi@users.noreply.github.com>
2025-02-25 18:03:10 +0100

    update animation page content -> Title + Start

--- src/components/ContentPage.tsx
+++ src/components/ContentPage.tsx
@@ -2,27 +2,52 @@ import Header from './Header';
 import '/src/assets/css/ContentPage.scss';
 import { RxCross1 } from 'react-icons/rx';
 import { useNavigate } from 'react-router-dom';
+import { useEffect, useRef } from 'react';
+import gsap from 'gsap';
+import Marquee from "react-fast-marquee";
 
 function ContentPage() {
     const navigate = useNavigate();
-    
+    const bgTopRef = useRef(null);
+    const contentRef = useRef(null);
+
+    useEffect(() => {
+        const tl = gsap.timeline();
+        tl.fromTo(
+            bgTopRef.current,
+            { y: -100 },
+            { duration: 1.5, y: 0, ease: "power3" }
+        );
+        tl.fromTo(
+            contentRef.current,
+            { y: 100 },
+            { duration: 1.5, y: 0, ease: "power3" },
+            "<"
+        );
+    }, []);
+
+
     return (
         <>
             {/* -------- HEADER -------- */}
             <Header />
             <main>
                 <section className='background'>
-                    <div className='bg-top'>
+                    <div ref={bgTopRef} className='bg-top'>
                         <img className='bg-top-img' src="/images/menu_bg1.jpeg" alt="background image" />
                     </div>
                 </section>
-                <section className='content'>
+                <section ref={contentRef} className='content' >
                     <img className='content-top' src="/images/menu1.png" alt="Lavande" />
-                    <RxCross1 className={`close-icon`} onClick={() => navigate('/')}/>
+                    <RxCross1 className={`close-icon`} onClick={() => navigate('/')} />
                     <div className='content-bot'>
-                        <h1>La lavande</h1>
+                        <Marquee className='titre' gradient={false} speed={100}>
+                            <h1>La lavande&nbsp;&nbsp;&nbsp;</h1>
+                            <h1>La lavande&nbsp;&nbsp;&nbsp;</h1>
+                            <h1>La lavande&nbsp;&nbsp;&nbsp;</h1>
+                        </Marquee>
                         <div className='content-text'>
-                        <img src="/images/yellow_circle.png" alt="yellow_circle" />
+                            <img src="/images/yellow_circle.png" alt="yellow_circle" />
                             <h2>Les propriétés de la lavande</h2>
                             <h3>Nous utilisons la lavande fine particulièrement pour ses propriétés apaisantes et son parfum délicat. <strong> Cet ingrédient est au cœur de notre gamme « Soin Corps »</strong>, par exemple dans la composition de notre eau de Cologne et bain moussant. Faisant partie d’un complexe de différentes huiles essentielles, la lavande se trouve également dans certains de nos produits d’aromachologie.</h3>
                         </div>
