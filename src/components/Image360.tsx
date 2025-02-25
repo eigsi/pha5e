3488992069af46e470ae6dbacd230cac08765576
eigsi <126054825+eigsi@users.noreply.github.com>
2025-02-25 12:54:05 +0100

    update textures switch

--- src/components/Image360.tsx
+++ src/components/Image360.tsx
@@ -1,26 +1,89 @@
 import '/src/assets/css/Image360.scss';
-import { useLoader } from '@react-three/fiber';
-import { TextureLoader, BackSide } from 'three';
+import { BackSide } from 'three';
 import { OrbitControls } from '@react-three/drei';
 import ClickablePoint from './ClickableCircle';
+import { a, useSpring } from '@react-spring/three';
+import { useEffect, useState } from 'react';
+import { easeCubicInOut } from 'd3-ease';
 
-function Image360({ onDragStart, onDragEnd }: { onDragStart: () => void; onDragEnd: () => void })  {
-  const texture = useLoader(TextureLoader, '/images/360view_1.jpg');
+function Image360({ textureIndex, textures, onDragStart, onDragEnd }: { textureIndex: number; textures: any[]; onDragStart: () => void; onDragEnd: () => void }) {
+
+  const [currentTextureIndex, setCurrentTextureIndex] = useState(textureIndex);
+  const baseTexture = textures[textureIndex];  // BASE TEXTURE
+  const nextTexture = textures[(textureIndex + 1) % textures.length];   // NEW TEXTURE
+
+  // ACCUMULATED ROTATION TO KEEP CONTINUITY
+  const [accumulatedRotation, setAccumulatedRotation] = useState(0);
+
+  // SPRING FOR THE GROUP ROTATION
+  const [rotationSpring, rotationApi] = useSpring(() => ({
+    rotationY: accumulatedRotation,
+    config: { duration: 750, easing: easeCubicInOut },
+  }));
+
+  // Spring FOR OPACITY OF THE TRANSITION SPHERE
+  const [opacitySpring, opacityApi] = useSpring(() => ({
+    opacity: 0,
+    config: { duration: 750, easing: easeCubicInOut },
+  }));
+  
+
+  useEffect(() => {
+    if (textureIndex !== currentTextureIndex) {
+      // 1) START OPACITY 0->1
+      opacityApi.start({ opacity: 1 });
+      // 2 ) START ROTATION +90° 
+      rotationApi.start({
+        rotationY: accumulatedRotation + Math.PI / 2,
+        config: { duration: 750, easing: easeCubicInOut},
+        onRest: () => { 
+          setTimeout(() => {  // DELAY TO CHANGE TEXTURE
+            // UPDATE TEXTURE
+            setCurrentTextureIndex(textureIndex);
+            // RESET OPACITY OF THE TRANSITION SPHERE
+            opacityApi.set({ opacity: 0 });
+            // UPDATE ACCUMULATED ROTATION
+            const newAccumulated = accumulatedRotation + Math.PI / 2;
+            setAccumulatedRotation(newAccumulated);
+            rotationApi.set({ rotationY: newAccumulated });
+          }); 
+        },
+      });
+    }
+  }, [textureIndex, currentTextureIndex, accumulatedRotation, opacityApi, rotationApi]);
+
+
+
 
   const handlePointClick = () => {
     console.log('Point clicked');
   };
   return (
     <>
-      <mesh>
-        <sphereGeometry args={[500, 60, 60]} />
-        <meshBasicMaterial map={texture} side={BackSide} />
-      </mesh>
+      <a.group rotation-y={rotationSpring.rotationY}>
+        {/* BASE SPHERE*/}
+        <mesh>
+          <sphereGeometry args={[500, 60, 60]} />
+          <meshBasicMaterial map={baseTexture} side={BackSide} />
+        </mesh>
+        {/* TAMPORARY SPHERE WITH NEW TEXTURE*/}
+        <a.mesh>
+          <sphereGeometry args={[500, 60, 60]} />
+          <a.meshBasicMaterial
+            map={nextTexture}
+            side={BackSide}
+            transparent
+            opacity={opacitySpring.opacity}
+          />
+        </a.mesh>
+      </a.group>
+
       <OrbitControls
         enableZoom={false}
         rotateSpeed={-0.5}
         onStart={onDragStart}
-        onEnd={onDragEnd} />
+        onEnd={onDragEnd}
+      />
       <ClickablePoint position={[0, -10, -50]} onClick={handlePointClick} />
     </>
 
--- src/components/InteractiveView360.tsx
+++ src/components/InteractiveView360.tsx
@@ -3,14 +3,32 @@ import { Canvas } from '@react-three/fiber';
 import Image360 from './Image360';
 import Header from './Header';
 import { useState } from 'react';
+import { useLoader } from '@react-three/fiber';
+import { TextureLoader } from 'three';
 
 function InteractiveView360() {
+
   const [isDragging, setIsDragging] = useState(false);
+
+  const textures = useLoader(TextureLoader, [
+    '/images/360view_1.jpg',
+    '/images/360view_2.jpg',
+    '/images/360view_3.jpg'
+  ]);
+
+  const [textureIndex, setTextureIndex] = useState(0);
+  // CHANGE TEXTURE
+  const handleTextureChange = () => {
+    setTextureIndex(prevIndex => (prevIndex + 1) % textures.length);
+  };
+
   return (
     <>
       {/* -------- 360 PLANE -------- */}
       <Canvas className="Canvas360">
         <Image360
+          textureIndex={textureIndex}
+          textures={textures}
           onDragStart={() => setIsDragging(true)}
           onDragEnd={() => setIsDragging(false)}
         />
@@ -18,7 +36,10 @@ function InteractiveView360() {
 
       {/* -------- YELLOW CIRCLE -------- */}
       <div className='overlay-circle-shape'>
-        <div className={`overlay-circle ${isDragging ? "dragging" : ""}`}>
+        <div
+          className={`overlay-circle ${isDragging ? "dragging" : ""}`}
+          onClick={handleTextureChange}
+        >
           <h2>CHANGE DE VUE</h2>
         </div>
       </div>
